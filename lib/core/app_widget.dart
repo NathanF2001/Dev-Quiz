7662ce4584b8073d1f55358c2eac32545e686387
Nathan Freitas <nathanfreitas403@gmail.com>
2021-04-19 21:30:34 -0300

    first commit

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:quiz_flutter/home/home_page.dart';
+import 'package:quiz_flutter/splash/splash_page.dart';
+
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
